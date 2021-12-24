881c2ab4e568f09cc852fc71850eda04061f5250
huyenma91 <phuong.pham.2101@hcmut.edu.vn>
2021-12-24 16:57:54 +0700

    change build

--- src/index.js
+++ src/index.js
@@ -17,7 +17,15 @@ app.use(express.static(path.join(__dirname,'build')));
 //     password: 'matkhaulagi',
 //     database: 'du_lich',
 // })
-
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+   //and remove cacheing so we get the most recent appointments
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+   });
 const db = mysql.createPool({
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b7b7839688788e',
